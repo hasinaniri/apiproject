f92c20e27036eea2617693367e6cc804cda1a788
hasinaniri <hasinanirina0012@gmail.com>
2026-08-21 15:56:23 +0300

    Feat/cors (#4)
    * fix(auth): fix middleware logic and update initialization code
    
    * feat(config): configure CORS, update env template, and export database schema

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
 import StudentRoutes from "./routes/StudentRoute";
 import AuthRoute from "./routes/AuthRoute";
+
 import { errorHandler, notFoundHandler } from "./middlewares/errorHandler";
 import { authMiddleware } from "./middlewares/authMiddleware";
 
 const app: Application = express();
 
 app.use(express.json());
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+}))
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send("Hello World");
